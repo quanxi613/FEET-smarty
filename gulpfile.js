28f4620a5577ddb5d95cab340c55272cb4b84aba
quanxi613 <quanxi613@126.com>
2016-04-08 16:18:09 +0800

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -41,14 +41,14 @@ gulp.task('connect', function () {
 
 
   gulp.watch('./app/src/css/**/*.less', ['less', reload]);
-  gulp.watch('./app/Tpl/**/*.tpl', ['tpl', reload]);
+  gulp.watch('./app/tpl/**/*.tpl', ['tpl', reload]);
   gulp.watch('./app/src/js/**/*.js', ['js', reload]);
   gulp.watch('./app/src/js/**/*.hbs', ['jstpl', reload]);
 
   gulp.watch([
     './app/**/*',
     '!./app/src/**/*',
-    '!./app/Tpl/**/*',
+    '!./app/tpl/**/*',
     '!./app/less/**/*'
     ], ['copy', reload]);
 
@@ -59,7 +59,7 @@ gulp.task('copy', function () {
   return gulp.src([
     './app/**/*',
     '!./app/src/**/*',
-    '!./app/Tpl/**/*',
+    '!./app/tpl/**/*',
     '!./app/less/**/*'
     ])
     .pipe(gulp.dest(config.dist))
@@ -72,9 +72,9 @@ gulp.task('mock', function () {
 });
 
 gulp.task('tpl', function () {
-  return gulp.src('./app/Tpl/**/*')
-    .pipe($.changed(config.dist + 'Tpl'))
-    .pipe(gulp.dest(config.dist + 'Tpl'));
+  return gulp.src('./app/tpl/**/*')
+    .pipe($.changed(config.dist + 'tpl'))
+    .pipe(gulp.dest(config.dist + 'tpl'));
 });
 
 gulp.task('img', function () {
@@ -101,31 +101,7 @@ gulp.task('less', function () {
 
   return gulp.src(['./app/src/css/**/*.less', '!./app/src/css/Common/include/*.less'])
     .pipe($.plumber())
-    .pipe($.watchLess('./app/src/css/**/*.less'))
     .pipe($.changed(config.dist + 'src/css', {extension: '.css'}))
-    .pipe($.sourcemaps.init())
-    .pipe($.less())
-    .pipe($.postcss([
-      require('autoprefixer-core')({browers: AUTOPREFIXER_BROWSERS})
-    ]))
-    .pipe($.sourcemaps.write())
-    .pipe($.if(config.uglify, $.csso()))
-    .pipe(gulp.dest(config.dist + 'src/css'))
-    .pipe($.plumber.stop())
-    .pipe(reload({stream: true}));
-});
-
-gulp.task('less-build', function () {
-  var AUTOPREFIXER_BROWSERS = [
-    'ie >= 8',
-    'ff >= 30',
-    'chrome >= 34',
-    'safari >= 7',
-    'opera >= 23'
-  ];
-
-  return gulp.src(['./app/src/css/**/*.less', '!./app/src/css/Common/include/*.less'])
-    //.pipe($.changed(config.dist + 'src/css', {extension: '.css'}))
     .pipe($.sourcemaps.init())
     .pipe($.less())
     .pipe($.postcss([
@@ -172,12 +148,10 @@ gulp.task('jstpl', function () {
     .pipe(gulp.dest(config.dist + 'src/js'));
 });
 
-//gulp.task('bower', ['moyeless', 'moyejs', 'moyefont', 'laydate', 'ueditor']);
-
 gulp.task('clean', require('del').bind(null, ['dist']));
 
-var build = ['tpl', 'jstpl', 'less-build', 'css', 'js', 'img'];
-var local = ['tpl', 'jstpl', 'less', 'css', 'js', 'img'];
+var build = ['tpl', 'jstpl', 'less', 'css', 'js', 'img'];
+//var local = ['tpl', 'jstpl', 'less', 'css', 'js', 'img'];
 
 //本地调试
 gulp.task('default', function (callback) {
@@ -185,7 +159,7 @@ gulp.task('default', function (callback) {
     'clean',
     'mock',
     'copy',
-    local,
+    build,
     'connect',
     callback
     );
